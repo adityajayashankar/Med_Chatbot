6b52198822c1be65017f18d85d6e31d81ca20d8f
Aditya Jayashanakar <adityajayashankar@gmail.com>
2025-08-01 23:22:32 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="med_chatbot",
+    version="0.1.0",
+    author="Aditya Jayashankar",
+    author_email="adityajayashankar@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
